de475a818c33a6c0c4cf06e1be43571b1548acf1
Jolty <juplagon@gmail.com>
2017-02-20 11:13:18 +0100

    3dsx branch: cleanup

--- twlnand-side/clean and make.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-make clean
-make
-patch_ndsheader_dsiware.py TWLapp-hb.nds --mode dsi --maker 01 --code TWLD --title "TWLOADER-TWL" --out TWLapp.nds
-make_cia --srl="TWLapp.nds"
-copy "TWLapp.cia" "../7zfile/sdroot/TWLoader - TWLNAND side.cia"
-pause
